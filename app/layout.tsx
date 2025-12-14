cde95b35c481fed6ed5f0efd12b19dd22d8c7ecf
Hardik <hs489819@gmail.com>
2025-12-14 23:15:48 +0530

    intial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Pixelify_Sans, Inter } from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +11,17 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const gameFont=Pixelify_Sans({
+  variable: "--font-game",
+  subsets: ["latin"],
+});
+
+const inter=Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +36,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${gameFont.variable} ${inter.variable} antialiased`}
       >
         {children}
-      </body>
+      </body> 
     </html>
   );
 }
